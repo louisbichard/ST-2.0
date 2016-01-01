42d31b9053a96d2f3459a32892a97d8f6cd51df0
Lou Bichard <lou.bichard@mudano.com>
2016-01-01 20:05:18 +0000

    removed sorter

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -2,7 +2,6 @@ import { Component } from 'angular2/core';
 import { CORE_DIRECTIVES } from 'angular2/common';
 import { RouterLink } from 'angular2/router';
 import { DataService } from '../../services/data.service';
-import { Sorter } from '../../utils/sorter';
 import { SortByDirective } from '../../directives/sortby.directive';
 import { CapitalizePipe } from '../../pipes/capitalize.pipe';
 
@@ -18,7 +17,6 @@ export class CustomersComponent {
   title: string;
   customers: any[] = [];
   filteredCustomers: any[] = [];
-  sorter: Sorter;
   tasks: any[] = [{
       name: "A task",
       date: "A due date",
@@ -37,12 +35,6 @@ export class CustomersComponent {
         .subscribe((customers:any[]) => {
           this.customers = this.filteredCustomers = customers;
         });
-
-    this.sorter = new Sorter();
-  }
-
-  sort(prop: string) {
-      this.sorter.sort(this.filteredCustomers, prop);
   }
 
 }
